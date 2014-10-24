45953fc4049d694ca21a50b791ca125fa8b62f3c
nickmiste <nickmiste@gmail.com>
2014-10-23 21:19:34 -0400

    Bug Fixes

--- SpaceItems/src/main/java/nickmiste/FloatItemTask.java
+++ SpaceItems/src/main/java/nickmiste/FloatItemTask.java
@@ -1,6 +1,8 @@
 package nickmiste;
 
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.WitherSkull;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -9,28 +11,26 @@ import org.bukkit.util.Vector;
 public class FloatItemTask extends BukkitRunnable
 {
 	Item item;
-	double vecX;
-	double vecY;
-	double vecZ;
 	
 	public FloatItemTask(Item item)
 	{
 		this.item = item;
-		vecX = item.getVelocity().getX();
-		vecY = item.getVelocity().getY();
-		vecZ = item.getVelocity().getZ();
 	}
 	
 	public void run()
 	{
 		if (!item.isValid())
+		{	
+			if (item.getVehicle() != null)
+				item.getVehicle().remove();
 			this.cancel();
-		if (true)//(SQSpace.isInSpace(item))
+			return;
+		}
+		if (isInSpace(item))
 		{
 			if (item.getVehicle() == null)
 			{
-				Location loc = item.getLocation();
-				WitherSkull skull = item.getWorld().spawn(loc, WitherSkull.class);
+				WitherSkull skull = item.getWorld().spawn(item.getLocation().add(0, -1, 0), WitherSkull.class);
 				skull.setDirection(new Vector(0, 0, 0));
 				skull.setPassenger(item);
 		  	}
@@ -39,4 +39,32 @@ public class FloatItemTask extends BukkitRunnable
 			item.getVehicle().remove();
 		
 	}
+	
+	private static boolean isInSpace(Entity e)
+	{
+		final Location l = e.getLocation();
+		boolean air1 = true;
+		boolean air2 = true;
+		final int x = l.getBlockX();
+		final int y = l.getBlockY();
+		final int z = l.getBlockZ();
+		final World w = l.getWorld();
+		int height = 40;
+ 
+		for (int i = 0; i < height; i++) 
+		{
+			final int id1 = w.getBlockTypeIdAt(x, y + i + 1, z);
+			final int id2 = w.getBlockTypeIdAt(x, y - i, z);
+			if (id1 != 0) 
+				air1 = false;
+			
+			if (id2 != 0) 
+			{
+			air2 = false;
+			}
+		}
+		if ((!air1) && (!air2))
+			return false;
+		return true;
+}
 }
--- SpaceItems/src/main/java/nickmiste/Main.java
+++ SpaceItems/src/main/java/nickmiste/Main.java
@@ -1,15 +1,9 @@
-
 package nickmiste;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Horse;
-import org.bukkit.entity.WitherSkull;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.ItemDespawnEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
@@ -23,9 +17,7 @@ public final class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onDisable() 
 	{
-		// This is a little edit by george for git purposes
-		// Remove later plz
-		// Nickmiste saw this commit
+		
 	}
 
 	@EventHandler
@@ -33,75 +25,4 @@ public final class Main extends JavaPlugin implements Listener {
 	{
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new FloatItemTask(event.getEntity()), 5, 0);
 	}
-
-	@EventHandler
-	public void onItemPickup(PlayerPickupItemEvent event)
-	{
-		if (event.getItem().getVehicle() != null)
-			event.getItem().getVehicle().remove();
-	}
-
-	@EventHandler
-	public void onItemDespawn(ItemDespawnEvent event) 
-	{
-		if (event.getEntity().getVehicle() != null)
-			event.getEntity().getVehicle().remove();
-	}
-
-	// Debug Method
-	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event)
-	{
-		for (int i = 0; i < event.getPlayer().getWorld().getEntities().size(); i++)
-			if (event.getPlayer().getWorld().getEntities().get(i) instanceof WitherSkull)
-				event.getPlayer().getWorld().spawn(event.getPlayer().getWorld().getEntities().get(i).getLocation(), Horse.class);
-	}
-
-	// Unused
-
-	/*@EventHandler
-	public void onItemPickup(PlayerPickupItemEvent event)
-	{
-		final List<Entity> entities = event.getPlayer().getWorld().getEntities();
-		final Location playerLoc = event.getPlayer().getLocation();
-		for (int i = 0; i < entities.size(); i++)
-		{
-			if (entities.get(i) instanceof WitherSkull && entities.get(i).getLocation().distance(playerLoc) <= 5)
-			{
-				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() 
-				{
-					  public void run() 
-					  {
-						  for (int i = 0; i < entities.size(); i++)
-							  if (entities.get(i) instanceof WitherSkull && entities.get(i).getLocation().distance(playerLoc) <= 5)
-								  entities.get(i).remove();
-					  }
-				}, 1200);
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onItemDespawn(ItemDespawnEvent event)
-	{
-		final List<Entity> entities = event.getEntity().getWorld().getEntities();
-		final Location playerLoc = event.getEntity().getLocation();
-		for (int i = 0; i < entities.size(); i++)
-		{
-			if (entities.get(i) instanceof WitherSkull && entities.get(i).getLocation().distance(playerLoc) <= 5)
-			{
-				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() 
-				{
-					  public void run() 
-					  {
-						  for (int i = 0; i < entities.size(); i++)
-							  if (entities.get(i) instanceof WitherSkull && entities.get(i).getLocation().distance(playerLoc) <= 5)
-								  entities.get(i).remove();
-					  }
-				}, 1200);
-			}
-		}
-	}
-	*/
-
 }
